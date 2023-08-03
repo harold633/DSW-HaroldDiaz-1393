2323703c9aa087e56fedbf69e651f7963cbca765
harold633 <137244201+harold633@users.noreply.github.com>
2023-08-03 08:15:06 -0500

    Fase4
    Fase4

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function procesarRespuesta(){
+    let total=5;
+    let puntos=0;
+
+    let myForm=document.forms["formulario"];
+    let respuestasCorrectas=["c","a","b","b","a"];
+
+    for (let i=1; i<= total; i++){
+        if ( myForm["r" + i].value==null ||   myForm["r" + i].value=='') {
+                alert('Favor responder todas las preguntas');
+                return false;
+            } else {
+                if (myForm["r"+i].value === respuestasCorrectas[i-1])
+                puntos++
+            }
+    }
+    let resultado =document.getElementById('resultado');
+    resultado.innerHTML='Obtuviste'+puntos+'puntos de '+ total + 'posibles';
+}
